3ac58912539be55ee8c091a93734a64051d254df
João Marcos <54422421+jmarcos589@users.noreply.github.com>
2021-11-30 09:32:52 -0300

    Update Comentários

--- logica.rb
+++ logica.rb
@@ -1,22 +1,23 @@
-expressao = '<<.<<..>><>><.>.>.<<.>.<.>>>><>><>>'
+expressao = '<<.<<..>><>><.>.>.<<.>.<.>>>><>><>>' # Inicializando variáveis
 num = expressao.size
 cont_areia = 0
 cont_diamante = 0
-i = 0
-expressao.size.times do |i|
+
+num.times do |i| # Laço para iterar as areias
   if expressao[i] == '.'
-    cont_areia += 1
+    cont_areia += 1 # Contador de Areias na expressão
   end
 end
-expressao = expressao.delete('.')
+expressao = expressao.delete('.') # Deletando Areias
 
-while expressao.size != 4 do
-  num.times do |i|
-    if expressao[i] == '>' && expressao[i-1] == '<'
-      cont_diamante += 1
-      expressao = expressao.sub('<>', '')
+while expressao.size != 4 do # Laço para iterar diamantes e refazer processo até acabar todos 
+  num.times do |i| 
+    if expressao[i] == '>' && expressao[i-1] == '<' 
+      cont_diamante += 1 # Contador de Diamantes na expressão
+      expressao = expressao.sub('<>', '') # Deletando Diamantes
     end
   end
 end
+
 puts "A sobra da expressão foi ", expressao
 puts "Foram retiradas %d areias e %d diamantes" % [cont_areia, cont_diamante]
