6123e79dd768746e902b79741820a61d3ef8be12
João Marcos <54422421+jmarcos589@users.noreply.github.com>
2021-11-29 23:17:53 -0300

    Retirando diamantes da expressão

--- logica.rb
+++ logica.rb
@@ -1,14 +1,22 @@
 expressao = '<<.<<..>><>><.>.>.<<.>.<.>>>><>><>>'
+num = expressao.size
 cont_areia = 0
 cont_diamante = 0
-
+i = 0
 expressao.size.times do |i|
   if expressao[i] == '.'
     cont_areia += 1
   end
+end
+expressao = expressao.delete('.')
 
-  if expressao[i] == '>' && expressao[i-1] == '<'
-    cont_diamante += 1
+while expressao.size != 4 do
+  num.times do |i|
+    if expressao[i] == '>' && expressao[i-1] == '<'
+      cont_diamante += 1
+      expressao = expressao.sub('<>', '')
+    end
   end
 end
-puts "A expressão tem %d areias e %d diamantes" % [cont_areia, cont_diamante]
+puts "A sobra da expressão foi ", expressao
+puts "Foram retiradas %d areias e %d diamantes" % [cont_areia, cont_diamante]
